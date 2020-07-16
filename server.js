c83d1075fe077551d1f36de0e048c5fe9d83fa62
Jorge Alejandro Nepomuceno Orozco <68257538+WaveGun115@users.noreply.github.com>
2020-07-16 02:14:45 -0500

    Se actualizo el codigo
    xdd
    
    Co-Authored-By: Jorge Alejandro Nepomuceno Orozco <68257538+wavegun115@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,15 +1,55 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const { 
+  Client, 
+  MessageEmbed, 
+  VoiceChannel
+} = require("discord.js");
+const bot = new Client();
 
-client.on("ready", () => {
-    console.log("Link Start!");
+const token = "NzMzMTQ1NDUyNDYzMTI4NjE3.Xw_Gmw.Nthi4CeQxt9hnH34JrGSVQEBqAk";
+const comando = ">";
+
+
+bot.on("ready", () => {
+    console.log(`Link Start!`);
  });
- 
- client.on("message", (message) => {
-   if(message.content.startsWith("-sc music")) {
-     message.channel.send("Comando system call para musica!!");
+
+ bot.on("message", (message) => {  
+
+  let datos = message.content.substring(comando.length).split(" ");
+  if (datos[0] === "play") 
+  {
+    if (datos[1] === undefined) 
+    {
+      message.channel.send("Necesitas un nombre o URL de la cancion");
+    }else
+    {
+      message.channel.send("Reproduciendo cancion...");
+    }
+    console.log("DATO 1: " + datos[1]);
+  }else
+  {
+    message.channel.send("Comando incorrecto");
+  }
+
+   if (message.content === ">help") {
+     const embed = new MessageEmbed()
+     .setColor("BLUE")
+     .addField("Buscar cancion", ">sc play")
+     .addField("Pausar cancion", ">sc pause");
+     const embed2 = new MessageEmbed()
+     .setColor("BLUE")
+     .addField("Ayuda de comancdos", ">help");
+     message.channel.send(`----- Comandos aceptados para Cardinal System -----`);
+     message.channel.send(`    * Modulo de ayuda *`);
+     message.channel.send(embed2);
+     message.channel.send(`    * Modulo de musica *`);
+     message.channel.send(embed);
    }
- 
+
+   if (message.content === "hola") {
+     message.channel.send(`Hola ${message.author} Bienvenido`);
+   }
+
  });
  
- client.login("NzMzMTQ1NDUyNDYzMTI4NjE3.Xw-6Cg.pPhCDnVH0qKdhXV4L0cidss7HhE");
+ bot.login(token);
